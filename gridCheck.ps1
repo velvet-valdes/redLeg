c5bae3a69f08322e737d04f8fb4d855b473dc564
velvet-valdes <valdestr@gmail.com>
2018-04-07 17:01:19 -0400

    Cast JSON file into PSObject
    Casting the file into a PSObject and loading variables to work with for sanity check

--- gridCheck.ps1
+++ gridCheck.ps1
@@ -1,5 +1,7 @@
 ﻿# Parse the fireDirectionalControl file to load the JSON into an object
+
 $missionParameters = (Get-Content -Raw -Path C:\Users\Administrator\Documents\WindowsPowerShell\redleg\fireDirectionalControl.json | ConvertFrom-Json) #don't forget to un-hardcode that path!
+
 
 # Concatenate the search base 
 
@@ -8,10 +10,15 @@ $searchBase01 = $missionParameters.search[1]
 $searchBase02 = $missionParameters.search[2]
 $searchBase = "DC=$searchBase00, OU=$searchBase01, OU=$searchBase02"
 
-Write-Host "Operations Directory Path:" $missionParameters.ops
-Write-Host "Payload Path:" $missionParameters.payload
-Write-Host "Advance Party Path:" $missionParameters.ap
-Write-Host "Active Directory Filter String:" $missionParameters.target
-Write-Host "Active Directory Search Base:" $searchBase
+# Set variables from the newly created object
+
+$opsDir = $missionParameters.ops
+$filterTarget = $missionParameters.target
+$advanceParty = $missionParameters.ap
+
+Write-Host "Operations Directory Path: $opsDir"
+Write-Host "Advance Party Path: $advanceParty"
+Write-Host "Active Directory Filter String: $filterTarget"
+Write-Host "Active Directory Search Base: $searchBase"
 
 Read-host -Prompt "Press Enter to exit"
