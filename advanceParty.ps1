3f85a342e265ddf943f48aed5b79311945b0567f
velvet-valdes <valdestr@gmail.com>
2018-06-05 05:48:02 -0400

    version update
    update xmr-stak to 2.4.4

--- advanceParty.ps1
+++ advanceParty.ps1
@@ -23,7 +23,7 @@ function payload ($destination) {
   # Get xmr-stak from github.
 
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-  Invoke-WebRequest -Uri https://github.com/fireice-uk/xmr-stak/releases/download/2.4.3/xmr-stak-win64.zip -OutFile $destination
+  Invoke-WebRequest -Uri https://github.com/fireice-uk/xmr-stak/releases/download/2.4.4/xmr-stak-win64.zip -OutFile $destination
 
 }
 
